baf4fb47e5b9fd2c1cd0f72b44db739222d614be
Eigram Eclarin <egeclarin@up.edu.ph>
2022-03-29 03:09:59 +0800

    Cart Total & Quantity
    displays total of each item but does not update

--- pages/order/addItem.php
+++ pages/order/addItem.php
@@ -25,21 +25,22 @@
             $itemPrice = $rowS['item_RetailPrice'];
             $itemName = $rowS['item_Name'];
 
-            if(empty($_SESSION['cart'])) {
+            if(!isset($_SESSION['cart'])) {
                 //if no cart yet
                 $itemTotal = $itemPrice * 1;
                 $_SESSION['cart'] = array(array("name" => $itemName, "qty" => 1, "price" => $itemPrice, "total" => $itemTotal));
             } else {
-                foreach ($_SESSION['cart'] as $eachItem) {
-                    if ($eachItem['name'] == $itemName) {
-                        $eachItem['qty'] += 1;
-                        $eachItem['total'] = $eachItem['qty'] * $eachItem['price'];
-                        $wasFound = true;
-                        break;
+                if (array_key_exists($itemID, $_SESSION['cart'])) {
+                    foreach ($_SESSION['cart'] as $eachItem) {
+                        if ($eachItem['name'] == $itemName) {
+                            $eachItem['qty'] += 1;
+                            $eachItem['total'] = $eachItem['qty'] * $eachItem['price'];
+                            $wasFound = true;
+                            break;
+                        }
                     }
-                }
-
-                if ($wasFound == false) { //if cart exists, but product not added yet
+                } else {
+                    $itemTotal = $itemPrice * 1;
                     array_push($_SESSION['cart'], array("name" => $itemName, "qty" => 1, "price" => $itemPrice, "total" => $itemTotal));
                 }
             }
