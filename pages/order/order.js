8403d0a434a04a1d8d81c030158ef7d152ac6b42
Eigram Eclarin <egeclarin@up.edu.ph>
2022-04-17 20:35:28 +0800

    updated modal

--- pages/order/order.js
+++ pages/order/order.js
@@ -1,34 +1,3 @@
-function showItems(categ) { //display items by categ
-    $.ajax({
-        url: 'getItem.php?categ='+categ,
-        success: function(html) {
-        var display = document.getElementById('list');
-        display.innerHTML = html;
-        }
-    });
-};
-
-function showResult(str) {
-  $.ajax({
-      url: 'getItem.php?q='+str,
-      success: function(html) {
-          var search = document.getElementById('livesearch');
-          search.innerHTML=html;
-          search.style.border="1px solid #A5ACB2"; 
-      }
-  });
-};
-
-function showResult(str) {
-  $.ajax({
-    url: 'getItem.php?q='+str,
-    success: function(html) {
-      var search = document.getElementById('datalistOptions');
-      search.append(html);
-    }
-  });
-};
-
 //change qty
 function changeQty(getID, getQty) {
     var dataString = "action=update&itemID="+getID+"&qty="+getQty;
@@ -65,7 +34,7 @@ function totalPrice() {
 //calculateChange
 function calculateChange(money) {
     var change = money - document.getElementById("totalOrder").value;
-    document.getElementById("change").value = change + ".00";
+    document.getElementById("change").value = change.toFixed(2);
 
     if (change < 0) {
         document.getElementById('pay').disabled = true;
--- pages/order/order.php
+++ pages/order/order.php
@@ -30,7 +30,7 @@
 
         switch($u) {
             case 'u':
-                $update = "Updated item quantity.";
+                $update = "Already in cart. Updated item quantity.";
                 break;
             case 'i':
                 $update = "Item added to cart.";
@@ -149,6 +149,9 @@
     <link rel="stylesheet" href="order.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
     <!-- JQUERY/BOOTSTRAP -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -171,7 +174,7 @@
 
             /*------ SEARCH ITEM ------*/  
             $("#searchItem").autocomplete({
-                source: plang,
+                source: plang.slice(0,5),
             });
 
             /*------ ENTER KEY - SUBMIT ------*/  
@@ -278,7 +281,7 @@
                     <!-- display item information -->
                     <div class="row text-center border-bottom p-2 mt-2 fs-5">
                         <!-- item name -->
-                        <div class="col-4 fw-bold text-wrap">
+                        <div class="col-4 text-start fw-bold text-wrap">
                             <?php echo $iName ?> 
                         </div>
 
@@ -292,7 +295,7 @@
                             <form action="updateItem.php?action=update&itemID=<?php echo $iID ?>" method="post" class="mb-0">
                                 <div class="input-group mb-0">
                                     <input type="number" name="qty" id="qty" class="form-control" value="<?php echo $iQty ?>" max="<?php echo $itemStock ?>" aria-describedby="button-addon2">
-                                    <button class="btn btn-outline-secondary" type="button" id="qtyUpdate">&#8635;</button>
+                                    <button class="btn btn-outline-secondary" type="submit" id="qtyUpdate">&#8635;</button>
                                 </div>
                             </form>
                             <p id="stocks" <?php echo $style ?>> Stocks left: <?php echo $itemStock ?> </p>
@@ -369,8 +372,7 @@
                     <!-- modal header -->
                     <div class="modal-header">
                         <h5 class="modal-title" id="receiptLabel">Receipt</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
+                        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close">
                         </button>
                     </div>
                     <!-- end of modal header -->
@@ -378,7 +380,7 @@
                     <!-- modal body -->
                     <div class="modal-body">
                         <div class="container-fluid">
-                            <div class="row w-100 m-0">
+                            <div class="row w-100 m-0 pb-3">
                                 <div class="col pl-0">
                                     <div clas="container">
                                     <?php
@@ -395,11 +397,11 @@
                                     ?>
                                         <div class="row">
                                             <div class="col-8">
-                                                <h6 class="fs-5 mb-0 <?php echo $margin ?>"> <?php echo $rowCart["itemName"] ?> </h6>
-                                                <small class="text-muted mb-2"> <?php echo $rowCart["itemPrice"]?>.00 x <?php echo $rowCart["quantity"] ?></small>
+                                                <h6 class="mb-0 <?php echo $margin ?>"> <?php echo $rowCart["itemName"] ?> </h6>
+                                                <small class="text-muted mb-2"> <?php echo $rowCart["itemPrice"]?> x <?php echo $rowCart["quantity"] ?></small>
                                             </div>
                                             <div class="col">
-                                                <h6 class="fs-5 fw-bold mb-3 float-right <?php echo $margin ?>"> <?php echo $rowCart["itemTotalP"] ?>.00 </h6>
+                                                <h6 class="fw-bold mb-3 float-right <?php echo $margin ?>"> <?php echo $rowCart["itemTotalP"] ?> </h6>
                                             </div>
                                         </div>
                                     <?php
@@ -410,33 +412,40 @@
                                 </div>
                                 <div class="col border-left pr-0">
                                 <form action="updateItem.php?action=order" method="post">
-                                    <div class="form-floating mb-3 row">
-                                        <label for="total" class="col-sm-2 col-form-label">Total</label>
-                                        <div class="col-sm-10">
-                                            <input type="text" readonly class="form-control-plaintext h2" id="totalOrder" name="total" value="<?php echo $totalPrice.'.00' ?>">
+                                    <div class="row mb-3">
+                                        <div class="col-2">
+                                            <span class="fs-5"> Total </span>
+                                        </div>
+                                        <div class="col">
+                                            <input type="text" class="form-control fw-bold text-end fs-2" id="totalOrder" name="total" value="<?php echo $totalPrice ?>">
                                         </div>
                                     </div>
-                                    <div class="form-floating mb-3 row">
-                                        <label for="moneyInput" class="col-sm-2 col-form-label">Money</label>
+
+                                    <div class="row mb-3">
+                                        <div class="col-2">
+                                            <p class="mb-0 pt-2"> Money </p>
+                                        </div>
                                         <div class="col-sm-10">
-                                            <input type="text" class="form-control border font-weight-bold" name="money" id="moneyInput" placeholder="0.00" onkeyup="calculateChange(this.value)">
+                                            <input type="text" class="form-control border text-end " autocomplete="off" name="money" id="moneyInput" placeholder="0" onkeyup="calculateChange(this.value)">
                                         </div>
                                     </div>
-                                    <div class="form-floating mb-3 row">
-                                        <label for="change" class="col-sm-2 col-form-label">Change</label>
+                                    <div class="row mb-3">
+                                        <div class="col-2">
+                                            <p class="mb-0 pt-2"> Change </p>
+                                        </div>
                                         <div class="col-sm-10">
-                                            <input type="text" readonly class="form-control-plaintext font-weight-bold" name="change" id="change" value="" placeholder="0.00">
+                                            <input type="text" class="form-control text-end " name="change" id="change" value="" placeholder="0">
                                         </div>
                                     </div>
                                     <button class="w-100 mb-2 btn btn-lg rounded-4 btn-primary" name="pay" id="pay" type="submit">Pay</button>
-                                    <button type="button" class="w-100 py-2 mb-2 btn btn-outline-primary rounded-4" data-dismiss="modal">Close</button>
+                                    <button type="button" class="w-100 py-2 mb-2 btn btn-outline-danger rounded-4" data-dismiss="modal">Close</button>
                                 </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <!-- end of modal body -->
-                    
+
                     </div>
                 </div>
                 </div>
