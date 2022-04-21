0509443f45839f4c3e14a95e081c956f1ea28821
jc-sanjuan <90907121+jc-sanjuan@users.noreply.github.com>
2022-04-21 16:58:14 +0800

    removed supplier.js connection

--- pages/supplier/navbar.php
+++ pages/supplier/navbar.php
@@ -6,7 +6,7 @@
 
     ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script type="text/javascript" src="supplier.js"></script> 
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     
     <link rel="stylesheet" href="style.css">
@@ -70,15 +70,6 @@
           <li class="nav-item w-100">
               <a href="supplieritem.php" class="nav-link active text-light pl-4"> Items </a>
           </li>
-            <!--  <a href="pending.php" class="nav-link dropdown-toggle text-light pl-4" id="navbarDropDown" role="button" data-bs-toggle="dropdown" > Suppliers </a>
-
-               <ul class="dropdown-menu w-100" aria-labelledby="navbarDropdown">
-                <li><a href="#" class="dropdown-item text-light pl-4 p-2"> Orders </a> </li>
-                <li><a href="#" class="dropdown-item text-light pl-4 p-2"> Deliveries </a> </li>
-              </ul>
-
-          </li>-->
-
 
         
       </ul>
