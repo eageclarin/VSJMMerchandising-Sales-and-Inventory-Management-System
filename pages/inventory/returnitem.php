a39f2183bc58ceec971e159b43dda4bc24b909e9
cutiiiaaannn <90824154+cutiiiaaannn@users.noreply.github.com>
2022-04-22 23:13:21 +0800

    Update returnitem.php

--- pages/inventory/returnitem.php
+++ pages/inventory/returnitem.php
@@ -4,11 +4,13 @@
 	<title> Return Items </title>
 	<script src="myjs.js" type="text/javascript"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style1.css">
+
 
 </head>
 <body>
-<?php //include 'navbar.html'; ?>
+
+<?php include 'navbar.html'; ?>
 <?php
 
 //error_reporting(0);
@@ -33,86 +35,98 @@ if(isset($_POST['submit'])){
     }
     else
     {
-        echo "Records added successfully.";
+        echo "";
     }
 
 }
 ?>
-<div class= "returnform">
-	<form action = "./returnitem.php" method="post" id="form">
-		<p>
-			Item ID: 
-				<?php
-					$server = "localhost:3306";
-							$user = "root";
-							$pass = "";
-							$db = "VSJM";
-							$conn = mysqli_connect($server, $user, $pass, $db);
-							if(!$conn) die(mysqli_error($conn));
-					$query = "SELECT * from item";
-					$result = mysqli_query($conn,$query);
-								if(mysqli_num_rows($result) > 0){
-									echo "<select name='item_ID'>";
-										while($row = mysqli_fetch_assoc($result)){
-											echo "<option value='".$row['item_ID']."'>"
-											.$row['item_ID']."</option>";
+<div class = "page">
+	<div class = "ret2">
+		<div class = "table">
+		<?php
+		$server = "localhost:3306";
+		$user = "root";
+		$pass = "";
+		$db = "VSJM";
+		$conn = mysqli_connect($server, $user, $pass, $db);
+		
+		if(!$conn) die(mysqli_error($conn));
+		$sql = "SELECT * FROM return_item;";                                    
+		$result = mysqli_query($conn,$sql);
+		$resultCheck = mysqli_num_rows($result);
+			   
+		echo "<table> 
+				<tr>
+					<th> ID </th>
+					<th> Returned Quantity </th>
+					<th> Reason </th>
+				</tr>";
+
+		if ($resultCheck>0){
+			while ($row = mysqli_fetch_assoc($result)) {
+					echo "
+					<tr>
+					<td>" .$row['item_ID']. "</td>   
+					<td>" .$row['item_ReturnedQuan']. "</td>  
+					<td>" .$row['item_Reason'] . "</td> 
+					</tr>
+					";
+			}
+		}       
+		mysqli_close($conn);
+		echo "</table>";
+
+		?>
+		</div>
+	</div>
+	
+	<div class= "ret">
+		
+		<form action = "./returnitem.php" method="post" id="form">
+			<p>
+				Item ID: 
+					<?php
+						$server = "localhost:3306";
+								$user = "root";
+								$pass = "";
+								$db = "VSJM";
+								$conn = mysqli_connect($server, $user, $pass, $db);
+								if(!$conn) die(mysqli_error($conn));
+						$query = "SELECT * from item";
+						$result = mysqli_query($conn,$query);
+									if(mysqli_num_rows($result) > 0){
+										echo "<select name='item_ID'>";
+											while($row = mysqli_fetch_assoc($result)){
+												echo "<option value='".$row['item_ID']."'>"
+												.$row['item_ID']."</option>";
+											}
+											echo "</select><br>";
 										}
-										echo "</select><br>";
-									}
-							mysqli_close($conn);
-				?>
-		</p>
-		<p>
-			Returned Item Quantity:
-			<input type = "text" name = "item_ReturnedQuan" id="item_ReturnedQuan">
-		</p>
-		<p>
-			Reason:
-			<select name="item_Reason" id="item_Reason" style="height:30px;">
-            <option value="Excess" >Excess quantity</option>
-            <option value="Defect">Item has a defect</option>
-            <option value="Wrong_item"> Wrong item/s bought </option>
-        </select>
-		</p>
-		<input type="submit" name="submit" value="submit">
-		<button type="button" onclick="location.href='inventory.php'">Go back </button>
-	</form>
-</div>
-
-<div class = "returned_table">
-	<?php
-$server = "localhost:3306";
-							$user = "root";
-							$pass = "";
-							$db = "VSJM";
-							$conn = mysqli_connect($server, $user, $pass, $db);
-							if(!$conn) die(mysqli_error($conn));
-$sql = "SELECT * FROM return_item;";                                    
-$result = mysqli_query($conn,$sql);
-$resultCheck = mysqli_num_rows($result);
-       
-echo "<table> 
-        <tr>
-            <th> ID </th>
-            <th> Returned Quantity </th>
-            <th> Reason </th>
-        </tr>";
-
-if ($resultCheck>0){
-    while ($row = mysqli_fetch_assoc($result)) {
-            echo "
-			<tr>
-            <td>" .$row['item_ID']. "</td>   
-            <td>" .$row['item_ReturnedQuan']. "</td>  
-            <td>" .$row['item_Reason'] . "</td> 
-			</tr>
-			";
-    }
-}       
-mysqli_close($conn);
-echo "</table>";
-
-?>
+								mysqli_close($conn);
+					?>
+			</p>
+			<p>
+				Returned Item Quantity:
+				<input type = "text" name = "item_ReturnedQuan" id="item_ReturnedQuan" required>
+			</p>
+			<p>
+				Reason:
+				<select name="item_Reason" id="item_Reason" style="height:30px;">
+				<option value="Excess quantity" >Excess quantity</option>
+				<option value="Item has a defect">Item has a defect</option>
+				<option value="Wrong item/s brought"> Wrong item/s bought </option>
+				
+			</select>
+			</p>
+			
+			<input type="submit" name="submit" value="submit">
+			
+		</form>
+		
+	</div>
+	
+	
+	
 </div>
 
 </body>
