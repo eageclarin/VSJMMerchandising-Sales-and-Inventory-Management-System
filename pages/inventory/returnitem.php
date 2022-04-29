ba659c1482de4ff161208aa8fcbe54ca88f43e18
cutiiiaaannn <90824154+cutiiiaaannn@users.noreply.github.com>
2022-04-29 20:35:57 +0800

    Update returnitem.php

--- pages/inventory/returnitem.php
+++ pages/inventory/returnitem.php
@@ -3,8 +3,9 @@
 <head>
 	<title> Return Items </title>
 	<script src="myjs.js" type="text/javascript"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="style1.css">
+<link rel="stylesheet" href="returnstyle.css">
 
 
 </head>
@@ -12,8 +13,9 @@
 
 <?php include 'navbar.html'; ?>
 <?php
+include_once '../../env/conn.php';
 
-//error_reporting(0);
+error_reporting(0);
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 	$db = mysqli_connect("localhost","root","","VSJM");
 
@@ -21,13 +23,14 @@ if(!$db)
 {
     die("Connection failed: " . mysqli_connect_error());
 }
+
 if(isset($_POST['submit'])){
-	$item_ID = $_POST['item_ID'];
+	$item_Name = $_POST['item_Name'];
 	$item_ReturnedQuan = $_POST['item_ReturnedQuan'];
 	$item_Reason = $_POST['item_Reason'];
-	
-	$insert = mysqli_query($db,"INSERT INTO return_item ". "(item_ID, item_ReturnedQuan, item_Reason) ". "
-			  VALUES('$item_ID', '$item_ReturnedQuan', '$item_Reason')");
+	$itemReturn_Date = $_POST['itemReturn_Date'];
+	$insert = mysqli_query($db,"INSERT INTO return_item ". "(item_Name, item_ReturnedQuan, item_Reason, itemReturn_Date) ". "
+			  VALUES('$item_Name', '$item_ReturnedQuan', '$item_Reason', '$itemReturn_Date')");
 			  
 	if(!$insert)
     {
@@ -41,51 +44,69 @@ if(isset($_POST['submit'])){
 }
 ?>
 <div class = "page">
-	<div class = "ret2">
-		<div class = "table">
+	<div class = "table">
+	<h2>  List of Returned Items </h2>
+	<h3> Number of returned items: <?php $server = "localhost:3306";
+		$user = "root";
+		$pass = "";
+		$db = "VSJM";
+		$conn = mysqli_connect($server, $user, $pass, $db);
+			
+		if(!$conn) die(mysqli_error($conn)); 
+		$query = "SELECT SUM(item_ReturnedQuan) FROM return_item;";
+		$result = mysqli_query($conn, 'SELECT SUM(item_ReturnedQuan) AS item_ReturnedQuan_sum FROM return_item'); 
+		$row = mysqli_fetch_assoc($result); 
+		$sum = $row['item_ReturnedQuan_sum'];
+		echo $sum;
+		?></h3>
+		 
 		<?php
 		$server = "localhost:3306";
 		$user = "root";
 		$pass = "";
 		$db = "VSJM";
 		$conn = mysqli_connect($server, $user, $pass, $db);
-		
+			
 		if(!$conn) die(mysqli_error($conn));
 		$sql = "SELECT * FROM return_item;";                                    
 		$result = mysqli_query($conn,$sql);
-		$resultCheck = mysqli_num_rows($result);
-			   
+		$resultCheck = mysqli_num_rows($result);			
+			
 		echo "<table> 
 				<tr>
-					<th> ID </th>
+					<th> Return ID </th>
+					<th> Item Name </th>
 					<th> Returned Quantity </th>
 					<th> Reason </th>
+					<th> Date of Return </th>
 				</tr>";
 
 		if ($resultCheck>0){
 			while ($row = mysqli_fetch_assoc($result)) {
 					echo "
 					<tr>
-					<td>" .$row['item_ID']. "</td>   
+					<td>" .$row['return_ID']. "</td>  
+					<td>" .$row['item_Name']. "</td>
 					<td>" .$row['item_ReturnedQuan']. "</td>  
 					<td>" .$row['item_Reason'] . "</td> 
+					<td>" .$row['itemReturn_Date'] . "</td> 
 					</tr>
 					";
 			}
 		}       
 		mysqli_close($conn);
 		echo "</table>";
-
 		?>
-		</div>
+		
 	</div>
 	
-	<div class= "ret">
-		
+	<div class= "form">
+		<h2> Return Forms </h2>
 		<form action = "./returnitem.php" method="post" id="form">
+			<div class ="input">
 			<p>
-				Item ID: 
-					<?php
+				Item Name: 
+				<?php
 						$server = "localhost:3306";
 								$user = "root";
 								$pass = "";
@@ -95,39 +116,60 @@ if(isset($_POST['submit'])){
 						$query = "SELECT * from item";
 						$result = mysqli_query($conn,$query);
 									if(mysqli_num_rows($result) > 0){
-										echo "<select name='item_ID'>";
+										echo "<select name='item_Name' >";
 											while($row = mysqli_fetch_assoc($result)){
-												echo "<option value='".$row['item_ID']."'>"
-												.$row['item_ID']."</option>";
+												echo "<option value='".$row['item_Name']."'>"
+												.$row['item_Name']."</option>";
 											}
 											echo "</select><br>";
 										}
 								mysqli_close($conn);
 					?>
 			</p>
+			</div>
+			<div class = "input">
 			<p>
 				Returned Item Quantity:
 				<input type = "text" name = "item_ReturnedQuan" id="item_ReturnedQuan" required>
 			</p>
+			</div>
+			<div class = "input">
 			<p>
 				Reason:
-				<select name="item_Reason" id="item_Reason" style="height:30px;">
+				<select name="item_TReason" id="item_TReason" style="height:30px;" onchange="mhie();" required >
+				
+				<option value="" id="reason">--Select Reason--</option>
 				<option value="Excess quantity" >Excess quantity</option>
 				<option value="Item has a defect">Item has a defect</option>
-				<option value="Wrong item/s brought"> Wrong item/s bought </option>
-				
-			</select>
+				<option value="Wrong item/s bought"> Wrong item/s bought </option>
+				<option value="Wrong size bought"> Wrong size bougth </option>
+				<option value=""> Others </option>
+				</select>
+				<p>
+				Type reasons of return:
+				<input type="text" name= "item_Reason" id="item_Reason"  />
+				</p>
 			</p>
-			
+			</div>
+			<div class = "input">
+			<p>
+				Date of Return:
+				<input type="datetime-local" name="itemReturn_Date" id="itemReturn_Date" required>
+			</p>
+			</div>
+			<div class = "input">
 			<input type="submit" name="submit" value="submit">
-			
+			</div>
 		</form>
-		
 	</div>
 	
-	
-	
 </div>
-
+<script type="text/javascript">
+function mhie() {
+	var d = document.getElementById("item_TReason");
+	var displaytext=d.options[d.selectedIndex].text;
+	document.getElementById("item_Reason").value=displaytext;
+}  
+</script>
 </body>
 </html>
