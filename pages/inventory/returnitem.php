45434e3d42f9303ce64b0eb68acbfaab16b8e4ef
cutiiiaaannn <90824154+cutiiiaaannn@users.noreply.github.com>
2022-04-30 00:04:59 +0800

    Update returnitem.php

--- pages/inventory/returnitem.php
+++ pages/inventory/returnitem.php
@@ -1,3 +1,6 @@
+<?php
+include_once '../../env/conn.php';
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,13 +8,15 @@
 	<script src="myjs.js" type="text/javascript"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<link rel="stylesheet" href="returnstyle.css">
-
+<link rel="stylesheet" href="finalstyle.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
 </head>
 <body>
-
-<?php include 'navbar.html'; ?>
+<main class="h-100">
+<?php include 'navbar.php'; ?>
 <?php
 include_once '../../env/conn.php';
 
@@ -43,69 +48,59 @@ if(isset($_POST['submit'])){
 
 }
 ?>
-<div class = "page">
-	<div class = "table">
-	<h2>  List of Returned Items </h2>
-	<h3> Number of returned items: <?php $server = "localhost:3306";
-		$user = "root";
-		$pass = "";
-		$db = "VSJM";
-		$conn = mysqli_connect($server, $user, $pass, $db);
-			
-		if(!$conn) die(mysqli_error($conn)); 
-		$query = "SELECT SUM(item_ReturnedQuan) FROM return_item;";
-		$result = mysqli_query($conn, 'SELECT SUM(item_ReturnedQuan) AS item_ReturnedQuan_sum FROM return_item'); 
-		$row = mysqli_fetch_assoc($result); 
-		$sum = $row['item_ReturnedQuan_sum'];
-		echo $sum;
-		?></h3>
-		 
-		<?php
-		$server = "localhost:3306";
-		$user = "root";
-		$pass = "";
-		$db = "VSJM";
-		$conn = mysqli_connect($server, $user, $pass, $db);
-			
-		if(!$conn) die(mysqli_error($conn));
-		$sql = "SELECT * FROM return_item;";                                    
-		$result = mysqli_query($conn,$sql);
-		$resultCheck = mysqli_num_rows($result);			
-			
-		echo "<table> 
-				<tr>
-					<th> Return ID </th>
-					<th> Item Name </th>
-					<th> Returned Quantity </th>
-					<th> Reason </th>
-					<th> Date of Return </th>
-				</tr>";
-
-		if ($resultCheck>0){
-			while ($row = mysqli_fetch_assoc($result)) {
-					echo "
-					<tr>
-					<td>" .$row['return_ID']. "</td>  
-					<td>" .$row['item_Name']. "</td>
-					<td>" .$row['item_ReturnedQuan']. "</td>  
-					<td>" .$row['item_Reason'] . "</td> 
-					<td>" .$row['itemReturn_Date'] . "</td> 
-					</tr>
-					";
-			}
-		}       
-		mysqli_close($conn);
-		echo "</table>";
-		?>
-		
-	</div>
+<div class="container-fluid bg-light p-5">
+	<div class="row">
+		<div class="col-7">
 	
-	<div class= "form">
+		 
+			<?php
+			$server = "localhost:3306";
+			$user = "root";
+			$pass = "";
+			$db = "VSJM";
+			$conn = mysqli_connect($server, $user, $pass, $db);
+				
+			if(!$conn) die(mysqli_error($conn));
+			$sql = "SELECT * FROM return_item;";                                    
+			$result = mysqli_query($conn,$sql);
+			$resultCheck = mysqli_num_rows($result);			
+				
+			echo "<table> 
+					<tr>
+						<th> Return ID </th>
+						<th> Item Name </th>
+						<th> Returned Quantity </th>
+						<th> Reason </th>
+						<th> Date of Return </th>
+					</tr>";
+
+			if ($resultCheck>0){
+				while ($row = mysqli_fetch_assoc($result)) {
+						echo "
+						<tr>
+						<td>" .$row['return_ID']. "</td>  
+						<td>" .$row['item_Name']. "</td>
+						<td>" .$row['item_ReturnedQuan']. "</td>  
+						<td>" .$row['item_Reason'] . "</td> 
+						<td>" .$row['itemReturn_Date'] . "</td> 
+						</tr>
+						";
+				}
+			}       
+			mysqli_close($conn);
+			echo "</table>";
+			?>
+		</div>
+		
+	
+	
+	<div class="col bg-white border shadow-sm p-5" style="border-radius: 15px">
+	<div class="fs-3 fw-bold text-center"> RETURN ITEM </div>
+		<hr>
 		<h2> Return Forms </h2>
 		<form action = "./returnitem.php" method="post" id="form">
-			<div class ="input">
-			<p>
-				Item Name: 
+			<div class="form-group row"> 
+				<label for="item_Name" class="col-5 col-form-label fw-bold">Item Name:</label>
 				<?php
 						$server = "localhost:3306";
 								$user = "root";
@@ -116,7 +111,9 @@ if(isset($_POST['submit'])){
 						$query = "SELECT * from item";
 						$result = mysqli_query($conn,$query);
 									if(mysqli_num_rows($result) > 0){
-										echo "<select name='item_Name' >";
+									?>
+										<select name="item_Name" id="item" class="col-sm-10 form-select w-50" required>
+									<?php
 											while($row = mysqli_fetch_assoc($result)){
 												echo "<option value='".$row['item_Name']."'>"
 												.$row['item_Name']."</option>";
@@ -125,18 +122,14 @@ if(isset($_POST['submit'])){
 										}
 								mysqli_close($conn);
 					?>
-			</p>
 			</div>
-			<div class = "input">
-			<p>
-				Returned Item Quantity:
-				<input type = "text" name = "item_ReturnedQuan" id="item_ReturnedQuan" required>
-			</p>
+			<div class="form-group row mt-2">
+				<label for="item_ReturnedQuan" class="col-5 col-form-label fw-bold">Returned Qty:</label>
+				<input type = "text" name = "item_ReturnedQuan" id="item_ReturnedQuan" class="col-sm-10 form-control w-50"  required>
 			</div>
-			<div class = "input">
-			<p>
-				Reason:
-				<select name="item_TReason" id="item_TReason" style="height:30px;" onchange="mhie();" required >
+			<div class="form-group row mt-2">
+					<label for="item_TReason" class="col-5 col-form-label fw-bold">Reason:</label>
+				<select name="item_TReason" id="item_TReason" class="col-sm-10 form-select w-50" onchange="mhie();" required >
 				
 				<option value="" id="reason">--Select Reason--</option>
 				<option value="Excess quantity" >Excess quantity</option>
@@ -145,25 +138,27 @@ if(isset($_POST['submit'])){
 				<option value="Wrong size bought"> Wrong size bougth </option>
 				<option value=""> Others </option>
 				</select>
-				<p>
-				Type reasons of return:
-				<input type="text" name= "item_Reason" id="item_Reason"  />
-				</p>
-			</p>
 			</div>
-			<div class = "input">
-			<p>
-				Date of Return:
-				<input type="datetime-local" name="itemReturn_Date" id="itemReturn_Date" required>
-			</p>
+			<div class="form-group row mt-2">
+				<label for="item_Reason" class="col-5 col-form-label fw-bold">Other Reason:</label>
+				<input type="text" name= "item_Reason" id="item_Reason"  class="col-sm-10 form-control w-50"  required>
 			</div>
-			<div class = "input">
-			<input type="submit" name="submit" value="submit">
+			
+			<div class="form-group row mt-2">
+				<label for="itemReturn_Date" class="col-5 col-form-label fw-bold">Date of Return:</label>
+				<input type="datetime-local" name="itemReturn_Date" id="itemReturn_Date" class="col-sm-10 form-control w-50" required>
+			</div>
+			
+			<div class="form-group row">
+				<div class="col">
+					<input type="submit" name="submit" value="submit" class="btn btn-lg btn-success mt-3 w-100">
+				</div>
 			</div>
 		</form>
+		</div>
 	</div>
-	
 </div>
+</main>
 <script type="text/javascript">
 function mhie() {
 	var d = document.getElementById("item_TReason");
@@ -171,5 +166,6 @@ function mhie() {
 	document.getElementById("item_Reason").value=displaytext;
 }  
 </script>
+
 </body>
 </html>
