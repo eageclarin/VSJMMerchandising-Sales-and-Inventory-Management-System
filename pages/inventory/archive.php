9a0338a14c0aa5d4aa2238e9db4d6d6bab7d274e
ceejeygarcia <90835372+ceejeygarcia@users.noreply.github.com>
2022-06-01 18:45:02 +0800

    notif + undo to be added

--- pages/inventory/archive.php
+++ pages/inventory/archive.php
@@ -30,11 +30,7 @@ if (isset($_POST['edit'])) { //UPDATING INVENTORY
   $sqlUpdate = mysqli_query($conn,$updateStatus);
   $updateStatus = "UPDATE item SET item_Name = '$item_Name', item_unit='$item_Unit', item_Brand ='$item_Brand', item_Category = '$item_Category' WHERE item_ID = '$itemID';";
   $sqlUpdate = mysqli_query($conn,$updateStatus);
-  if ($sqlUpdate) {
-    echo "Update in inventory success <br/>";
-  } else {
-    echo mysqli_error($conn);
-  } 
+  
   unset($_POST['edit']);
   //header($url);
   //header("Location: ./inventory.php");
@@ -50,6 +46,9 @@ if (isset($_POST['edit'])) { //UPDATING INVENTORY
     <link rel="stylesheet" href="./style.css?ts=<?=time()?>">
     <script type="text/javascript" src="inventory.js"></script>
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
+		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
       <!-- CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -124,7 +123,7 @@ if (isset($_POST['edit'])) { //UPDATING INVENTORY
               </div> <!-- MODAL-BODY -->
               <div class="modal-footer pb-0">
                   <input type="hidden" name="url" value="salability.php">
-                  <input  type="submit" value="update" name="edit" class="form-control btn btn-primary" style="width:150px" > 
+                  <input  type="submit" value="Update" name="edit" class="form-control btn btn-primary" style="width:150px" > 
                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
               </div> <!-- MODAL FOOTER -->
             </form>  
@@ -358,7 +357,30 @@ if (isset($_POST['edit'])) { //UPDATING INVENTORY
           $('#editRetail').val(retail);
       });
 
+      	//Edit Notif
+
+			$(document).ready(function(){
+
+      $('#staticBackdrop').on('submit',function() {  
+      $.ajax({
+        url:'archive.php', 
+        data:$(this).serialize(),
+        type:'POST',
+        success:function(data){
+          console.log(data);
+          swal("Success!", "Item Updated!", "success");
+        },
+        error:function(data){
+          swal("Oops...", "Something went wrong :(", "error");
+        }
+        });
+        $("#staticBackdrop").delay(10000).fadeOut("slow");
+      });
+      });
       
+
+
+
     </script>
 
   </body>
