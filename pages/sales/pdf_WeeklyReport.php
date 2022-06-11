be949d8a9e8a551c54227346913e0a6997aecc14
rapharids <102048829+rapharids@users.noreply.github.com>
2022-06-11 15:04:47 +0800

    Update pdf_WeeklyReport.php

--- pages/sales/pdf_WeeklyReport.php
+++ pages/sales/pdf_WeeklyReport.php
@@ -36,18 +36,24 @@ $res = mysqli_query($conn, $sql);
     $date = date("Y-m-d");
     $sql = "SELECT YEAR(order_Date) AS year, MONTHNAME(order_Date) AS month, WEEK(order_Date) AS week, order_Date, COUNT(DISTINCT orders.order_ID) AS totalOrders, SUM(orderItems_Quantity) AS totalItems, SUM(orderItems_TotalPrice) AS totalSales FROM orders INNER JOIN order_items ON (orders.order_ID = order_items.order_ID) GROUP BY WEEK(order_Date), MONTH(order_Date), YEAR(order_Date)" ;
     $result = mysqli_query($conn,$sql);
+    $total = 0;
     $resultCheck = mysqli_num_rows($result);
     if ($resultCheck>0){
-      while ($row = mysqli_fetch_assoc($result)) {
-        $pdf->SetFont('Arial','',8);
+        while ($row = mysqli_fetch_assoc($result)) {
+            $pdf->SetFont('Arial','',8);
+            $y= $pdf ->GetY();
+            $pdf->Cell(95,8,"Week ".$row['week']. " (" .$row['month'].", ".$row['year'] .")",1,0,'C');
+            $pdf->Cell(95,8,$row['totalSales'],1,1,'C');
+            $total = $total + $row['totalSales'];
+            
+        }
         $y= $pdf ->GetY();
-        $pdf->Cell(95,8,"Week ".$row['week']. " (" .$row['month'].", ".$row['year'] .")",1,0,'C');
-        $pdf->Cell(95,8,$row['totalSales'],1,1,'C');
-        
-      }
-      $y1=$pdf ->GetY();
+        $pdf->SetFont('Arial','B',10);
+        $pdf->Cell(95,8,'',0,0,'L');
+        $pdf->Cell(95,8,'Grand Total:                       '.$total,0,1,'L');
+        $y1=$pdf ->GetY();
         $pdf ->SetY($y);
-      $pdf ->SetY($y1+10);
+        $pdf ->SetY($y1+10);
     } else {
         $pdf->SetFont('Arial','B',10);
         $pdf->Cell(0,8,"No Record Found",0,'C');
